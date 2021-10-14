8791d2323a00606ba51b3a603e4809c8487449ad
mfsaglam <mfsaglam1@gmail.com>
2021-10-14 15:08:37 +0300

    Initial Commit

---
+++ CustomSegmentedControl/CustomSegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegmentedControlApp.swift
+//  CustomSegmentedControl
+//
+//  Created by Fatih Sağlam on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
